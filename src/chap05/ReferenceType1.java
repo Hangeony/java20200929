185098516067913a8e617b78241bfc99d1eec5a0
Hangeony <geonhui95@naver.com>
2020-10-08 17:53:49 +0900

    20-10-08

--- src/chap05/ReferenceType1.java
+++ src/chap05/ReferenceType1.java
@@ -1,5 +1,13 @@
 package chap05;
 
 public class ReferenceType1 {
-
+	public static void main(String[] args) {
+		int i = 30;
+		int j = 50;
+		
+		String str = "java";
+		String str2 = "html";
+		
+		
+	}
 }
