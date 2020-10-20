194d650d997a2f6067a6319f8c61bf1433a376de
Hangeony <geonhui95@naver.com>
2020-10-20 17:43:28 +0900

    20-10-20

--- src/chap10/textbook/s100501/TryWithResourceExample.java
+++ src/chap10/textbook/s100501/TryWithResourceExample.java
@@ -4,7 +4,7 @@ public class TryWithResourceExample {
 	public static void main(String[] args) {
 		try(FileInputStream fis = new FileInputStream("file.txt")){
 			fis.read();
-			throw new Exception();
+			throw new Exception();    // 발생 시킬 수 있는 스트리먼트 
 		}
 		catch(Exception e) {
 			System.out.println("예외 처리 코드가 실행되었습니다.");
