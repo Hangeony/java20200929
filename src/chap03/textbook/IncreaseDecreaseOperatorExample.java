9ce8ff8479b9708f79824d6391ae25e6e2fa6d14
Hangeony <geonhui95@naver.com>
2020-11-03 16:38:37 +0900

    20-11-03

--- src/chap03/textbook/IncreaseDecreaseOperatorExample.java
+++ src/chap03/textbook/IncreaseDecreaseOperatorExample.java
@@ -7,17 +7,17 @@ public class IncreaseDecreaseOperatorExample {
 		int z;
 		
 		System.out.println("--------------");
-		x++;
-		++x;
+		x++; //11의 값이 저장
+		++x; //12의 값이 저장
 		System.out.println("x =" + x);
 		
 		System.out.println("--------------");
-		y--;
-		--y;
+		y--; //9의 값이 저장
+		--y; //8의 값이 저장
 		System.out.println("y =" + y);
 		
 		System.out.println("--------------");
-		z = x++;
+		z = x++; //12의 값을 불러오고 12가 저장되고 그뒤에 x의 값이 늘어남
 		System.out.println("z =" + z);
 		System.out.println("z =" + x);
 		
@@ -27,7 +27,7 @@ public class IncreaseDecreaseOperatorExample {
 		System.out.println("z =" + x);
 		
 		System.out.println("--------------");
-		z = ++x + y++;
+		z = ++x + y++; //23 
 		System.out.println("z =" + z);
 		System.out.println("x =" + x);
 		System.out.println("y =" + y);
